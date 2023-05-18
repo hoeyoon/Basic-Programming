57f024436bead148d310fd7444582a356b9de38f
hoeyoon <hoeiyoon57@gmail.com>
2023-05-19 03:21:35 +0900

    네 번째 문제

---
+++ Practice 2/N4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  float x;
+  printf("실수값 입력 ");
+  scanf("%f", &x);
+
+  printf("몫 %d\n", (int)x / 5);
+  printf("나머지 %f\n", x - ((int)x / 5) * 5);
+
+  return 0;
+}
