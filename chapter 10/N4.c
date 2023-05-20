174a108bdb143c8fc47da9ef0f41f8ef5a63a490
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-05-20 17:41:06 +0900

    네 번째 문제 하는중...

--- chapter 10/N4.c
+++ chapter 10/N4.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
 int main(){
-  char x[100];
+  char input[100];
+  char word[100];
+  int count = 0;
 
-  scanf("%s", x);
+  int i = 0;
+  while(intput[i] != '\0'){
+    scanf("%s", input);
+    i++;
+    count++;
+  }
+  i = 0;
+  while(input[i + count] != '\0'){
+    scanf("%s", word);
+    i++;
+  }
+
+  
   return 0;
 }
