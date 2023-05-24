c68d42f20c7915ff6c36e71400c4b42e45be6015
hoeyoon <hoeiyoon57@gmail.com>
2023-05-24 17:42:09 +0900

    두 번째 문제 하는중...

---
+++ chapter 11/N2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct{
+	char name[64];
+	int number[64];
+	char phone[64];
+	int age[64];
+} employee;
+
+int main(){
+	employee a[10] = {};
+	int x;
+	
+	
+	
+	return 0;
+}
