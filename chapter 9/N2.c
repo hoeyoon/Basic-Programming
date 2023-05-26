74a3f14b30726cc95994e78ce15ee58f2855bd8d
hoeyoon <hoeiyoon57@gmail.com>
2023-05-27 00:24:24 +0900

    두 번째 문제

---
+++ chapter 9/N2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int n[5];
+  int result = 0;
+
+  for(int i = 0; i < 5; i++){
+    scanf("%d", &n[i]);
+  }
+  for(int i = 0; i < 5; i++){
+    result+=n[i];
+  }
+  printf("%d\n", result);
+  return 0;
+}
