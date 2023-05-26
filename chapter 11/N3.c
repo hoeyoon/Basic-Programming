fd2e151f3a37e406e511e5ef5ab53506a90698f8
hoeyoon <hoeiyoon57@gmail.com>
2023-05-27 00:11:24 +0900

    두 번째 문제 하는중...

--- chapter 11/N3.c
+++ chapter 11/N3.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
 typedef struct{
-  float rnum;
-  float inum;
+  double rnum;
+  double inum;
 } complex_num;
 
+complex_num addcomplxe_num(complex_num a, complex_num b){
+  complex_num r;
+  r.rnum = a.rnum + b.rnum;
+  r.inum = a.inum + b.inum;
+  return r;
+}
+
 int main(){
-  complex_num x, y;
-  scanf("%f+%fi", &x.rnum, &x.inum);
-  scanf("%f+%fi", &y.rnum, &y.inum);
+  complex_num x, y, sum = {0 , 0};
+  scanf("%lf+%lfi", &x.rnum, &x.inum);
+  scanf("%lf+%lfi", &y.rnum, &y.inum);
 
-  double result_rnum, result_inum;
+  sum = addcomplxe_num(x, y);
 
-  result_rnum = x.rnum + y.rnum;
-  result_inum = x.inum + y.inum;
-
-  printf("%.2f+%.2fi\n", result_rnum, result_inum);
+  printf("%.2lf+%.2lfi\n", sum.rnum, sum.inum);
   return 0;
 }
