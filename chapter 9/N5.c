58fcf102a4c011646110d85f46ba29733add2bba
hoeyoon <hoeiyoon57@gmail.com>
2023-05-30 21:59:14 +0900

    다섯 번째  문제

---
+++ chapter 9/N5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int v[5];
+  for(int i = 0; i < 5; i++){
+    scanf("%d", &v[i]);
+  }
+
+  int* a = v;
+  for(int i = 4; i >= 0; i--){
+    printf("%d ", *(a+i));
+  }
+  printf("\n");
+  return 0;
+}
