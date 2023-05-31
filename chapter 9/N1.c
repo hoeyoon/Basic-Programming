54048bdc4097423ec8507870491e763bf198f428
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-05-31 15:35:19 +0900

    첫 번째 문제

--- chapter 9/N1.c
+++ chapter 9/N1.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 
 void swap(int* px, int* py){
-	int temp;
-	temp = *px;
-	*px = *py;
-	*py = temp;	
+  int temp;
+  temp = *px;
+  *px = *py;
+  *py = temp;
 }
 
 int main(){
-	int v[10];
-	int count = 0;
-	int x;
-	
-	while(count < 10){
-		scanf("%d", &x);
-		if(1 <= x && x <= 50){
-			v[count] = x;
-			count++;
-		} 
-		else {
-			continue;
-		}
-	}
-	for(int i = 0; i < 9; i++){
-		for(int j = i + 1; j < 10; j++){
-			if(*(v+i) > *(v+j)){
-				swap(&v[i], &v[j]);
-			}
-		}
-	}
-	for(int i = 0; i < 10; i++){
-		printf("%d ", v[i]);
-	}
-	printf("\n");
-	return 0;
+  int x, n[10];
+
+  int count = 0;
+  while(count < 10){
+    scanf("%d", &x);
+    if(1 <= x && x <= 50){
+      n[count] = x;
+      count++;
+    }
+    else{
+      continue;
+    }
+  }
+  for(int i = 0; i < 10; i++){
+    for(int j = 0; j < 10; j++){
+      if(n[i] < n[j]){
+        swap(&n[i], &n[j]);
+      }
+    }
+  }
+  for(int i = 0; i < 10 ; i++){
+    printf("%d ", n[i]);
+  }
+  printf("\n");
+  return 0;
 }
