9fef4a4ca3563179d535ca89e790ab56a536c845
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-06-05 21:19:37 +0900

    세 번째 문제

---
+++ chapter 13/N3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int* to_int_array(int* psize, char* x){
+  int* r;
+  r = (int*)malloc(strlen(x)*sizeof(int));
+  char* tok = strtok((char* x), " ");
+
+  int count = 0;
+
+  while(tok != '\0'){
+    if(x[i] == ' '){
+      r[count] = atoi(tok);
+      count++;
+    }
+    tok = strtok('\0', " ");
+  }
+  *psize = count;
+  return r;
+}
+
+int main(){
+	char str[256];
+	scanf("%[^\n]", str);
+	//printf("%s\n", str);
+	
+	int* array;
+	int size;
+	array = to_int_array( &size, str);
+	printf("size: %d\n", size);
+	for( int i = 0 ; i < size; i++){
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+	free(array);
+	return 0;
+}
