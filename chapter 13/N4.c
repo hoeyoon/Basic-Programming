9d2bfc6806fc377e0e133aa90bdb552c1b9974a9
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-06-05 22:04:03 +0900

    네 번째 문제 하는중...

--- chapter 13/N4.c
+++ chapter 13/N4.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* to_str(int* size, int* a){
+char* to_str(int* size, unsigned int* a){
   char* r;
-  r = (char*)malloc(a*sizeof(char));
-  
+  r = (char*)malloc((char*)a*sizeof(char));
+  for(int i = 0; i < 3; i++){
+    
+  }
   
   return r;
 }
+
 
 int main(){
 	unsigned int arr[3];
 	scanf("%u %u %u", arr, arr+1, arr+2);
 	
 	int str_size;
-	char *str = to_str( &str_size, arr);
+	char *str = to_str(&str_size, arr);
 	printf("str_size: %d\n", str_size);
 	printf("%s\n", str);
 	free(str);
