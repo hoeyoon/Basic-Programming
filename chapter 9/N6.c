acb47104999d8c2e3556bea4393d5e8bbf0831b2
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-06-09 11:08:27 +0900

    여섯 번째 문제

--- chapter 9/N6.c
+++ chapter 9/N6.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 
-void sort_array(int* v, int n, int m){
+void swap(int* a, int* b){
   int temp;
-  for(int i = 0 ; i < n; i++){
-    for(int j = 0; j < n; j++){
-      if(m == 0){
+  temp  = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void sort_array(int* v, int n, int m){
+  if(m == 0){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
         if(v[i] < v[j]){
-          temp = v[i];
-          v[i] = v[j];
-          v[j] = temp;
-        }
-      }
-      else if(m == 1){
-        if(v[i] > v[j]){
-          temp = v[i];
-          v[i] = v[j];
-          v[j] = temp;
+          swap(&v[i], &v[j]);
         }
       }
     }
   }
-  for(int i = 0; i < n; i++){
-    printf("%d ", *(v+i));
+  if(m == 1){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        if(v[i] > v[j]){
+          swap(&v[i], &v[j]);
+        }
+      }
+    }
   }
-  printf("\n");
 }
 
 int main(){
-  int n, m, v[10];
+  int v[10];
+  int n, m;
+
   scanf("%d %d", &n, &m);
-  
   for(int i = 0; i < n; i++){
     scanf("%d", &v[i]);
   }
+  
   sort_array(v, n, m);
+  for(int i = 0; i < n; i++){
+    printf("%d ", v[i]);
+  }
+  printf("\n");
   return 0;
 }
